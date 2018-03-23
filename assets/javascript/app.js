a7ff01c2446b68380c274cefd4f971b2eb7ec613
unknown <tberry6@gmail.com>
2018-03-22 17:02:20 -0700

    Added button creation functionality and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,33 @@
-var topics = ["Serval", "Hippo", "Racoon", "Penguin"];
+var topics = ["Serval", "Hippo", "Asian Small-clawed Otter", "Jaguar", "Crested Ibis", "Alpaca", "Raccoon", "Fennec Fox", "American Beaver", "Prairie Dog", "Lion", "Moose", "Shoebill", "Northern White-face Owl", "Eurasian Eagle Owl", "Margay", "Penguin", "Silver Fox", "Ezo Red Fox", "Campo Flicker", "Gray Wolf", "Reticulated Giraffe", "Golden Snub-nosed Monkey", "Brown Bear", "African Wild Dog"];
 
 var search;
 
-for (var i = 0; i < topics.length; i++) {
-    var button = $("<button>");
-    button.attr("id", topics[i]);
-    button.text(topics[i]);
-    $(".buttons").append(button);
+function buttons() {
+    $(".buttons").empty();
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        button.attr("id", topics[i]);
+        button.attr("type","button");
+        button.addClass("btn btn-primary");
+        button.text(topics[i]);
+        $(".buttons").append(button);
+    }
 }
+
 
 function getImages(response) {
     for(var i = 0; i < response.data.length; i++) {
-        imgUrl = response.data[i].images.fixed_height_still.url;
-        $(".images").append("<img src = '" + imgUrl + "'>");
+        var imgUrl = response.data[i].images.fixed_height_still.url;
+        var imgDisplay = $("<div class='display'>");
+        var rating = response.data[i].rating;
+        imgDisplay.append("<div>Rating: " + rating + "</div>");
+        imgDisplay.append("<img src = " + imgUrl + ">");
+        $(".images").append(imgDisplay);
     }
 
 }
+
+buttons();
 
 $("body").on("click", "button", function () {
     search = $(this).attr("id");
@@ -32,7 +44,6 @@ $("body").on("click", "button", function () {
 $("body").on("click", "img", function() {
     if(!$(this).hasClass("animated")) {
         var url = $(this).attr("src");
-        console.log(url);
         url = url.substring(0, url.length-6) + ".gif";
         $(this).attr("src",url);
         $(this).addClass("animated");
@@ -43,4 +54,12 @@ $("body").on("click", "img", function() {
         $(this).attr("src",url);
         $(this).removeClass("animated");
     }
+});
+
+$("#add-animal").on("click", function (event) {
+    event.preventDefault();
+
+    var input = $("#animal-input").val();
+    topics.push(input);
+    buttons();
 });
